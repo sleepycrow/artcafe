a6bb9046dc3462ddfc2c95df3e032599d73aa851
sleepycrow <sleepycrow@posteo.net>
2023-05-28 13:43:14 +0200

    Fix tests lmao I'm dumb sorry haha

--- test/pleroma/web/mastodon_api/views/status_view_test.exs
+++ test/pleroma/web/mastodon_api/views/status_view_test.exs
@@ -346,10 +346,6 @@ defmodule Pleroma.Web.MastodonAPI.StatusViewTest do
 
   test "an artwork activity" do
     note = insert(:artwork_activity)
-    object_data = Object.normalize(note, fetch: false).data
-    user = User.get_cached_by_ap_id(note.data["actor"])
-
-    convo_id = :erlang.crc32(object_data["context"]) |> Bitwise.band(Bitwise.bnot(0x8000_0000))
 
     status = StatusView.render("show.json", %{activity: note})
 
